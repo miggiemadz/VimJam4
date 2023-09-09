d1407c19bdff041dddab0537515d4945f1209924
PokeJofeJr4th <PokeJofeJr4th@gmail.com>
2023-09-09 18:18:57 -0400

    add boombox functionality

--- VimJam 4/Assets/Scripts/BoomBox.cs
+++ VimJam 4/Assets/Scripts/BoomBox.cs
@@ -4,17 +4,23 @@ using UnityEngine;
 
 public class BoomBox : MonoBehaviour
 {
-    public static float tempo = 80.0f;
+    public GameObject soundWavePrefab;
+    public float tempo = 80.0f;
+    public float repulsiveForce = 0.1f;
     float timer = 0.0f;
+    GameObject soundWave;
 
     void Start()
     {
         Debug.Log("BoomBox Placed");
+        soundWave = Instantiate(soundWavePrefab, transform.position, Quaternion.identity);
+        soundWave.transform.localScale = Vector3.zero;
     }
 
     void FixedUpdate()
     {
         timer += Time.fixedDeltaTime * tempo / 60;
+        soundWave.transform.localScale = Vector3.one * timer * 50;
         if (timer >= 1.0f)
         {
             Debug.Log("BOOM");
@@ -25,19 +31,25 @@ public class BoomBox : MonoBehaviour
         {
             transform.localScale = Vector3.one * 1.0f;
         }
-        float sound_radius = tempo * timer / 10;
+        float sound_radius = tempo * timer / 20;
         Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, sound_radius);
         foreach (Collider2D collider in colliders)
         {
             if (collider.gameObject.TryGetComponent<PoliceBehavior>(out var cop))
             {
                 Debug.Log("Found a cop!");
+                // this is zero when the cop is directly on the boombox
+                Vector3 copPosRelative = collider.transform.position - transform.position;
+                // cop moves more when closer to boombox, so movement is divided by magnitude
+                Vector3 copMovement = copPosRelative.normalized / copPosRelative.magnitude * repulsiveForce;
+                collider.gameObject.GetComponent<Rigidbody2D>().MovePosition(collider.transform.position + copMovement);
             }
         }
     }
 
     void OnDestroy()
     {
+        Destroy(soundWave);
         Debug.Log("BoomBox Destroyed");
     }
 }
