02b14af2de16003dc5755ce88b697f7571d776f1
PokeJofeJr4th <pokejofejr4th@gmail.com>
2023-09-10 14:36:16 -0400

    document Cooler behavior

--- VimJam 4/Assets/Scripts/Cooler.cs
+++ VimJam 4/Assets/Scripts/Cooler.cs
@@ -4,26 +4,33 @@ using UnityEngine;
 
 public class Cooler : MonoBehaviour
 {
+    // Floor item prefab
     public GameObject itemPrefab;
-
+    // Starting number of cans & bottles
     public ushort count = 6;
+    // Time from an item being picked up to the next one being spawned
     public float cooldown = 1.0f;
+    // cooldown timer. Null if there's an item near the cooler
     float? cooldownTimer = null;
 
     void FixedUpdate()
     {
+        // don't do anything if the cooler's empty
         if (count > 0)
         {
             if (cooldownTimer > 0.0f)
             {
+                // if we're waiting, tick down the timer
                 cooldownTimer -= Time.fixedDeltaTime;
             }
             else if (cooldownTimer != null)
             {
+                // if the timer is at zero, spawn an item
                 SpawnItem();
             }
             else
             {
+                // if the timer isn't set but there aren't any items nearby, start the timer
                 if (!CheckNearbyItem())
                 {
                     cooldownTimer = cooldown;
@@ -32,15 +39,24 @@ public class Cooler : MonoBehaviour
         }
     }
 
+    // spawn a new item, decrement the count, and reset the cooldown timer
     void SpawnItem()
     {
         GameObject newItem = Instantiate(itemPrefab, transform.position, Quaternion.identity);
         FloorItem newFloorItem = newItem.GetComponent<FloorItem>();
-        newFloorItem.itemType = ItemType.Bottle;
+        // choose a random item type
+        if ( Random.value > 0.5)
+        {
+            newFloorItem.itemType = ItemType.Can;
+        } else
+        {
+            newFloorItem.itemType = ItemType.Bottle;
+        }
         count--;
         cooldownTimer = null;
     }
 
+    // Check if there's a floor item nearby.
     bool CheckNearbyItem()
     {
         Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, 2.0f);
