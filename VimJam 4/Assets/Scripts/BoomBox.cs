acc7bedaf6c2d9790ab5de092f56b4e6ed2ca2d2
PokeJofeJr4th <PokeJofeJr4th@gmail.com>
2023-09-09 21:03:34 -0400

    add a little bit of the exploding cat script, fix prefab stuff that broke

--- VimJam 4/Assets/Scripts/BoomBox.cs
+++ VimJam 4/Assets/Scripts/BoomBox.cs
@@ -17,6 +17,4 @@ public class BoomBox : MonoBehaviour
     {
         Debug.Log(boomboxOn);
     }
-
-
 }
---
+++ VimJam 4/Assets/Scripts/ExplodingCat.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplodingCat : MonoBehaviour
+{
+    void Start() {
+        Debug.Log("There's an exploding cat!! Ahh!!!");
+    }
+}
